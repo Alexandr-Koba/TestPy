609854f834ec44eda985736a4ea7867355816a71
AlexandrKoba <babakoba@yandex.ru>
2022-12-29 00:09:44 +0700

    Commit Day3

---
+++ myTest/day/3 day/FunMap.py
@@ -0,0 +1,16 @@
+'''
+
+
+'''
+
+a = [-1, 2, -3, 4, 5]
+
+b = list(map(abs, a))
+print(b)
+
+c = ['Koba', 'Babakoba', 'ProKoba']
+x = list(map(str.upper, c))
+print(x)
+
+h = list(map(len, c))
+print(h)
