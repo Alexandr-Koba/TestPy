e0a86889d7a4beb33bc28ff8f30fb4cad8d24a6c
AlexandrKoba <babakoba@yandex.ru>
2023-01-02 19:26:03 +0700

    2 january night

---
+++ myTest/Examen/Footbol.py
@@ -0,0 +1,6 @@
+age = int(input())
+mf = input()
+if age >= 10 and age <= 15 and mf == 'f':
+    print("YES")
+else:
+    print("NO")
---
+++ myTest/day 2023/day1.py
@@ -0,0 +1,7 @@
+# Возведение в степень
+a = int(input())
+b = int(input())
+print(a ** b)
+
+
+
