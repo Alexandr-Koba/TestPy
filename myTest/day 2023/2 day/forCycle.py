be5a5057f5a7741b8cfa4773c1d6c7f3233438ff
AlexandrKoba <babakoba@yandex.ru>
2023-01-03 17:35:52 +0700

    Commit Koba 3january

---
+++ myTest/day 2023/2 day/forCycle.py
@@ -0,0 +1,34 @@
+for i in range(1, 4):
+    print(i)
+
+for y in range(4):
+    print('Привет babakoba')
+
+
+num = input()
+count = 0
+for a in num:
+    print(f"{count}, {a}")
+    count += 1
+print(f"Всего букв {count}")
+
+
+# Квадрат
+
+for i in range(5):
+    num = int(input())
+    print('Квадрат вашего числа равен:', num * num)
+print('Цикл завершен')
+
+# Task 1
+for i in range(10):
+    print('Python is awesome!')
+
+# Подщет через переменную цикла
+for i in range(10):
+    print(i, '-- Привет')
+
+# Если мы хотим начать с 1 а не с 0
+for i in range(10):
+    print(i + 1, '-- Привет')
+
