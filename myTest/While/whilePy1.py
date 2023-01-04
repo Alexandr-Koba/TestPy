c6450531fef1e4f7cb9f1cc5ae65f52aa9bb21b8
AlexandrKoba <babakoba@yandex.ru>
2023-01-04 21:45:52 +0700

    AnyKaty Commit

---
+++ myTest/While/whilePy1.py
@@ -0,0 +1,25 @@
+'''
+
+'''
+# используем for
+for i in range(11):
+    print(i)
+
+# используем while
+i = 0
+while i < 11:
+    print(i)
+    i += 1
+
+# Напишем программу, выводящую все числа, кратные 3, используя цикл for и while:
+
+# используем for
+for i in range(0, 100, 3):
+    print(i)
+
+# используем while
+i = 0
+while i < 100:
+    print(i)
+    i += 3
+
--- myTest/day 2023/3 day.py/forTask1.py
+++ myTest/day 2023/3 day.py/forTask1.py
@@ -1,4 +1,17 @@
 # Напишем программу, которая отсчитывает от 5 до 1, а затем выводит текст Взлетаем!!!:
-for i in range(5, 1):
+for i in range(1, 6):
     print(i, end=' ')
     print('Старт')
+
+# Отрицательная нумерация
+for j in range(5, 0, -1):
+    print(j, end = ' ')
+    print('start')
+
+
+# Подщет
+count = int(input())
+name = input()
+for k in range(count, 0, -1):
+    print(k, end=' ')
+    print(name)
